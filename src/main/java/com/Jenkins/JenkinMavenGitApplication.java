28ff7a45c95ed50760a33522c0b9667829b4058d
Sujit <jeet96211@gmail.com>
2023-04-09 20:33:33 +0530

    JenkinWithGit

---
+++ src/main/java/com/Jenkins/JenkinMavenGitApplication.java
@@ -0,0 +1,22 @@
+package com.Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JenkinMavenGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinMavenGitApplication.class, args);
+
+	}
+
+	
+	@RequestMapping("/")
+	public String msg() {
+		return "Started";
+	}
+}
